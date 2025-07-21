69841a36db302a65b072ec079ffb27150002c845
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2025-07-21 23:02:52 +0530

    Create script.js

---
+++ project_root/static/script.js
@@ -0,0 +1,23 @@
+function startVoice() {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  if (!SpeechRecognition) {
+    alert("Sorry, your browser doesn't support voice input.");
+    return;
+  }
+
+  const recognition = new SpeechRecognition();
+  recognition.lang = 'en-US';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+
+  recognition.start();
+
+  recognition.onresult = function(event) {
+    const transcript = event.results[0][0].transcript;
+    document.getElementById("user_input").value = transcript;
+  };
+
+  recognition.onerror = function(event) {
+    alert("Voice input error: " + event.error);
+  };
+}
